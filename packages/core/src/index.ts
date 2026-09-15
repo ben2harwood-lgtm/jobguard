bb631afabb7f8e3ac07d8a49c1b9c0b970fd32f3
ben2harwood-lgtm <ben2harwood@gmail.com>
2026-09-15 02:32:20 +0100

    feat(fees): add illustrative per-job statement

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@
 export const CORE_PACKAGE = "@jobguard/core" as const;
 export * from "./allocation.js";
 export * from "./fee.js";
+export * from "./fee-statement.js";
 export * from "./job.js";
 export * from "./capture.js";
 export * from "./money.js";
